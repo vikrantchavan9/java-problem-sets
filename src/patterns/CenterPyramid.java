8f69530d3deb8474f288e3cc95d55b07d0a9fbb0
Vikrant <vikrantchavan200@gmail.com>
2025-06-16 00:36:34 +0530

    added patterns/CenterPyramid.java

---
+++ src/patterns/CenterPyramid.java
@@ -0,0 +1,20 @@
+package src.patterns;
+
+public class CenterPyramid {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i =1; i <= n; i++) {
+            for (int s=5; s >= i; s--) {
+                System.out.print(" ");
+            }
+            for (int j=1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+}
