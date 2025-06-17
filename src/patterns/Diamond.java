5ac23d1530912a51eba5cf44685e2e3244aae90b
Vikrant <vikrantchavan200@gmail.com>
2025-06-17 22:20:40 +0530

    Diamond pattern added

---
+++ src/patterns/Diamond.java
@@ -0,0 +1,30 @@
+package src.patterns;
+
+public class Diamond {
+    public static void main(String[] args) {
+        int n = 3; // Height of the top half
+
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
+
+
